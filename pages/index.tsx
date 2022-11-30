f303a7d7ef5b6023b6d7ad4407b2c93e918d13a9
joaisa17 <pvpdaimer@gmail.com>
2022-11-30 16:04:57 +0100

    Example

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+/** Add your relevant code here for the issue to reproduce */
+export default function Home() {
+  return <>
+    <style>{`h1 { font-family: 'Roboto'; }`}</style>
+    <h1>Hello world</h1>
+  </>
+}
